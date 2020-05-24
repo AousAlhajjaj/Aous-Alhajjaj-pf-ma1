bc3495fffa4c81057992ce374ed46e3a0c2f1e10
AousAlhajjaj <63973537+AousAlhajjaj@users.noreply.github.com>
2020-05-24 21:50:58 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+// question 1
+
+var firstVar;
+
+
+// question 2
+
+
+var name = "aous";
+
+
+// question 3
+
+
+var x = 10;
+
+
+// question 4
+
+
+var division = 20 / 5;
+
+
+// question 5
+
+
+var myfavoriteanimal = "frog";
+typeof myfavoriteanimal
+
+
+// question 6
+
+
+var orderHasShipped = true;
+
+
+// question 7
+
+
+var orderHasShipped = window.confirm("Click yes, get true.  Click no, get false.");
+
+if (orderHasShipped === true) {
+  console.log("true");
+} else {
+  console.log("false");
+ }   
+ 
+ 
+
+// question 8
+    
+    
+
+for (var c = 0; c <10; c += 1) {
+    console.log(c);
+}
